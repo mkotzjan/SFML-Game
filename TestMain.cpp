95c971eeaeecd3f0f84fb9f769cf24dfc7953104
ChaosBelcebub <mich3ael@web.de>
2015-05-18 23:43:07 +0200

    First Version of my TestMain

---
+++ TestMain.cpp
@@ -0,0 +1,16 @@
+// TestMain
+// ChaosBelcebub, 2015
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen())
+  {
+    
+  }
+}
