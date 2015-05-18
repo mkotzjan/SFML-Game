32d84cbab7e74b14bee4f2217cacb00e7d0731f5
ChaosBelcebub <mich3ael@web.de>
2015-05-18 23:44:57 +0200

    Add function for close button

--- TestMain.cpp
+++ TestMain.cpp
@@ -11,6 +11,13 @@ int main()
 
   while (window.isOpen())
   {
-    
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
   }
 }
