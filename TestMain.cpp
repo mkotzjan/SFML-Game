130a6e71de4d37cb85e728e13f05950f4f740249
ChaosBelcebub <mich3ael@web.de>
2015-05-19 23:08:34 +0200

    test version for cmake

--- TestMain.cpp
+++ TestMain.cpp
@@ -1,29 +1,10 @@
 // TestMain
 // ChaosBelcebub, 2015
 
-#include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
-
-  while (window.isOpen())
-  {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed)
-      {
-        window.close();
-      }
-    }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-
-  return 0;
+  std::cout << "Success" << std::endl;
+  retun 0;
 }
