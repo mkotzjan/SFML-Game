978aaf767c0fc25b445cd08a0b1122af6cecb5d0
ChaosBelcebub <mich3ael@web.de>
2015-05-19 23:35:27 +0200

    add Space headerfile

---
+++ Space.h
@@ -0,0 +1,15 @@
+// Space.h
+// Author: Michael Kotzjan
+
+#ifndef SFML-GAME_SPACE_H_
+#define SFML-GAME_SPACE_H_
+
+#include <stdlib.h>
+
+class Space
+{
+  public:
+    // void add(
+};
+
+#endif // SFML-GAME_SPACE_H_
