74bd9b17a77a1bc9a0c67c6ca8ad3db48c11ebcb
ChaosBelcebub <mich3ael@web.de>
2015-05-19 23:34:52 +0200

    add Object header

---
+++ Object.h
@@ -0,0 +1,17 @@
+// Object.h
+// Author: Michael Kotzjan
+
+#ifndef SFML-GAME_OBJECT_H_
+#define SFML-GAME_OBJECT_H_
+
+#include <stdlib.h>
+
+// An Object
+class Object
+{
+  public:
+    size_t x;
+    size_t y;
+};
+
+#endif // SFML-GAME_OBJECT_H_
