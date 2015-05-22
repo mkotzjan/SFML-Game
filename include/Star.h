ed51ac9a9b4c1b28396105205b195e54431a806d
ChaosBelcebub <mich3ael@web.de>
2015-05-22 13:56:26 +0200

    copied object.h

---
+++ include/Star.h
@@ -0,0 +1,20 @@
+// Object.h
+// Author: Michael Kotzjan
+
+#ifndef OBJECT_H_
+#define OBJECT_H_
+
+#include <stdlib.h>
+#include "./PVector.h"
+
+// An Object
+class Object
+{
+  public:
+    PVector position;
+    
+    // Constructor
+    Object();
+};
+
+#endif // OBJECT_H_
