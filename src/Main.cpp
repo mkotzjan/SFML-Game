891fabef3e3b2a611b972dca9f669acea61c7f80
ChaosBelcebub <mich3ael@web.de>
2015-05-26 00:36:19 +0200

    added an actual window for testing

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,10 +2,29 @@
 // ChaosBelcebub, 2015
 
 #include <iostream>
-#include "Object.h"
+#include <SFML/Graphics.hpp>
+#include "Planet.h"
+#include "Star.h"
+#include "Space.h"
 
 int main()
 {
-  std::cout << "Success" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(500, 500), "SFML-Game");
+  
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
+
   return 0;
 }
